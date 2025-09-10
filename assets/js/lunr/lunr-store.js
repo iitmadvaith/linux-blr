dff0b05d71147488810e14f85b64e550b57f45e1
Lunarequest <luna.dragon@suse.com>
2025-09-10 05:22:51 +0000

    Deploying to gh-pages from @ iitmadvaith/linux-blr@9f4386f850745311e217d7d7f043be90b86ef129 🚀

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,26 @@
+/*
+Copyright (c) 2013-2020 Michael Rose and contributors
+MIT License
+From minimal-mistakes: https://github.com/mmistakes/minimal-mistakes/
+*/
+var store = [{
+    "title": "Archive",
+    "subtitle": "",
+    "excerpt": "",
+    "content": "",
+      "categories": null,
+      "date": null,
+      "tags": null,
+      "url": "https://dieghernan.github.io/archive",
+      "img": null
+    },{
+    "title": "A new start",
+    "subtitle": "A silly dream for the future",
+    "excerpt": "A bit of background",
+    "content": "A bit of background Before I get into the meet of this post I want to set a little context. I’ve been using Linux as my primary operating system since...",
+      "categories": ["announcements"],
+      "date": "2025-09-08",
+      "tags": ["getting started","LUG","announcement"],
+      "url": "https://dieghernan.github.io/announcements/2025/09/08/a-new-start.html",
+      "img": null
+    }]
